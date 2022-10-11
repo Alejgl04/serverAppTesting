4ac0542ed49819e2df814c164214e086b1d1f476
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-10-11 18:10:18 -0400

    database

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      *
      * @return void
      */
+    /*asdasd/*/
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
